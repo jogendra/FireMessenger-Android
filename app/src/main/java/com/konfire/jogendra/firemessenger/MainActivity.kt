0193e9c3dfe11b6417594a59399604086323f911
imjog <imjog24@gmail.com>
2018-08-27 16:25:18 +0530

    firebase setups

--- app/src/main/java/com/konfire/jogendra/firemessenger/MainActivity.kt
+++ app/src/main/java/com/konfire/jogendra/firemessenger/MainActivity.kt
@@ -3,6 +3,7 @@ package com.konfire.jogendra.firemessenger
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +12,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        register_button.setOnClickListener {
+            val email = email_edittext_register.text.toString()
+            val password = password_edittext_register.text.toString()
+
+            // Firebase Authentication
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener {
+                        if (!it.isSuccessful) return@addOnCompleteListener
+
+
+                    }
+        }
         presentLoginScreen()
     }
 
