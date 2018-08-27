cb6473477725275d3d9e97f976db95efe525a574
imjog <imjog24@gmail.com>
2018-08-27 16:39:02 +0530

    perform register user

--- app/src/main/java/com/konfire/jogendra/firemessenger/MainActivity.kt
+++ app/src/main/java/com/konfire/jogendra/firemessenger/MainActivity.kt
@@ -3,6 +3,7 @@ package com.konfire.jogendra.firemessenger
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -13,18 +14,30 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         register_button.setOnClickListener {
-            val email = email_edittext_register.text.toString()
-            val password = password_edittext_register.text.toString()
-
-            // Firebase Authentication
-            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
-                    .addOnCompleteListener {
-                        if (!it.isSuccessful) return@addOnCompleteListener
-
-
-                    }
+            performUserRegistration()
         }
         presentLoginScreen()
+    }
+
+    private fun performUserRegistration() {
+        val email = email_edittext_register.text.toString()
+        val password = password_edittext_register.text.toString()
+
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Please enter email/password!", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        // Firebase Authentication
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener {
+                    if (!it.isSuccessful) return@addOnCompleteListener
+
+                }
+                .addOnFailureListener {
+                    Toast.makeText(this, it.message, Toast.LENGTH_SHORT).show()
+                    return@addOnFailureListener
+                }
     }
 
     private fun presentLoginScreen() {
