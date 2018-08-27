28a39dcb3b636cf5cbc6d938405b45d48fb67aeb
imjog <imjog24@gmail.com>
2018-08-27 16:44:44 +0530

    Added basic login method

--- app/src/main/java/com/konfire/jogendra/firemessenger/LoginActivity.kt
+++ app/src/main/java/com/konfire/jogendra/firemessenger/LoginActivity.kt
@@ -3,6 +3,8 @@ package com.konfire.jogendra.firemessenger
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity: AppCompatActivity() {
 
@@ -10,5 +12,18 @@ class LoginActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_login)
+
+        login_button.setOnClickListener {
+            val email = email_edittext_login.text.toString()
+            val password = password_edittext_login.text.toString()
+
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener {
+
+                    }
+                    .addOnFailureListener {
+
+                    }
+        }
     }
 }
