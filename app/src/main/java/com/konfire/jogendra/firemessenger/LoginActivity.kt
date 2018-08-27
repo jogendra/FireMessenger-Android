ad86be6ed91f8cf80e017b235628250afb6f9468
imjog <imjog24@gmail.com>
2018-08-27 15:54:26 +0530

    basic main and login activity setups

---
+++ app/src/main/java/com/konfire/jogendra/firemessenger/LoginActivity.kt
@@ -0,0 +1,14 @@
+package com.konfire.jogendra.firemessenger
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
+
+class LoginActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_login)
+    }
+}
--- app/src/main/java/com/konfire/jogendra/firemessenger/MainActivity.kt
+++ app/src/main/java/com/konfire/jogendra/firemessenger/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.konfire.jogendra.firemessenger
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        presentLoginScreen()
+    }
+
+    private fun presentLoginScreen() {
+        already_have_account_textview.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
